49ceb4baa9a1e1d70b4fb1ef37aaf365a68d3313
agathman <agathman@outlook.com>
2022-04-11 23:39:01 -0500

    edits

--- app/views.py
+++ app/views.py
@@ -44,7 +44,7 @@ def index():
         elif request.form['check'] == 'event':
             event = Event_Order(request.form['category'], request.form['customer'], request.form['status'], request.form['eventTime'], request.form['theme'], request.form['eventDesc'],
                         request.form['delivery'], request.form['setup'], request.form['location'], request.form['restrictions'], request.form['address'], request.form['city'],
-                        request.form['zip'] , request.form['state'], 'Due after event')
+                        request.form['zip'] , request.form['state'])
             db.session.add(event)
             db.session.commit()
             flash('Success! Event Added.')
